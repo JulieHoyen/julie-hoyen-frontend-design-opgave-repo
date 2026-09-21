0556e11e10022fa901b40eba260c9a69ff60f526
JulieHoyen <juho0001@stud.ek.dk>
2026-09-21 14:02:51 +0200

    Update album details and enhance responsive styles

--- script.js
+++ script.js
@@ -5,20 +5,24 @@ themeSwitcher.addEventListener("click", () => {
   root.dataset.theme = root.dataset.theme === "dark" ? "light" : "dark";
 });
 
+const bandName = "Trouble Bunny";
+
+const songTitles = ["Who Ate the Cable? (wans't me)", "Midnight Zoomies", "Hay, hay, hay", "Under the Sofa naps", "Treat Emergency", "Fluffy Chaos", "No More Pellets", "Noizes at The Balcony", "Bunny Business", "Goodnight Ruben"];
+
 const songs = document.querySelector(".songs");
 
-songs.innerHTML = Array.from({ length: 10 })
+songs.innerHTML = songTitles
   .map(
-    (_, i) => `
-        <li class="song">
-          <div>
-            <p class="song-name">Song #${i + 1}</p>
-            <p class="band-name">Tetherform</p>
-          </div>
-          <p class="duration">3:${Math.floor(Math.random() * 60)
-            .toString()
-            .padStart(2, "0")}</p>
-        </li>
-`
+    (title, i) => `
+      <li class="song">
+        <div>
+          <p class="song-name">${title}</p>
+          <p class="band-name">${bandName}</p>
+        </div>
+        <p class="duration">3:${Math.floor(Math.random() * 60)
+          .toString()
+          .padStart(2, "0")}</p>
+      </li>
+    `,
   )
   .join("");
